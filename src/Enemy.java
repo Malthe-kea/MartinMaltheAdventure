2797cecad107b7a7824a067db9e8d2a442e5c941
MartinEllebyBerg <160470389+MartinEllebyBerg@users.noreply.github.com>
2024-03-20 19:45:37 +0100

    Smårettelser

--- src/Enemy.java
+++ src/Enemy.java
@@ -3,8 +3,6 @@ public class Enemy extends Item {
     //Attributer
     private int healthPoints;
 
-// Indhent et meleeWeapon fra klassen
-
     public Enemy(String shortName, String longName, int healthPoints, Weapon weapon) {
         super(shortName, longName);
         this.healthPoints = healthPoints;
