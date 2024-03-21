534b62d7fe0627b260520da92d73566dd2e2fadf
MartinEllebyBerg <160470389+MartinEllebyBerg@users.noreply.github.com>
2024-03-21 23:23:50 +0100

    Historie

--- src/Map.java
+++ src/Map.java
@@ -10,14 +10,14 @@ public class Map {
 
     //Methods
     public void createMap() {
-        Room room1 = new Room("Dragekammeret", "Lysende lavastrømme gløder om æggene under himlen af skæl og flamme, mens drager hviler i dvale.");
-        Room room2 = new Room("Alvekongens Salon", "Krystallignende blomster blomstrer over svajende trætoppe, mens elvere danser mellem lysende støvpartikler.");
-        Room room3 = new Room("Skytårnets Bibliotek", " Skyerne omslutter svævende bøger, mens stjernehimmel-arkiver og himmelblå reoler strækker sig mod uendeligheden af viden.");
+        Room room1 = new Room("The Dragon Chamber", "Glowing lava streams illuminate the eggs beneath the sky of scales and flames, while dragons rest in slumber.");
+        Room room2 = new Room("The Elf King's Salon)", "Crystal-like flowers bloom above swaying treetops, while elves dance among shimmering dust particles.");
+        Room room3 = new Room("Scyscraber of Magic", "The clouds envelop floating books, while starry sky archives and sky-blue shelves stretch towards the infinity of knowledge.");
         Room room4 = new Room("Troldmandens Laboratorium", "Boblende kedler og glimtende eliksirer omgiver en hvirvlende portal, hvor magiens pulserende hjerte skaber et kalejdoskopisk skuespil.");
-        Room room5 = new Room("Undervands Paladset", "Perlemorskoraller danner en labyrint omkring svævende slotte, hvor havets folk samles mellem tangskove og skinnende krystalgrotter");
+        Room room5 = new Room("The Prison Cell", "The cell of horror, might and nothing but endless screaming, terror and pain. Welcome to hell!");
         Room room6 = new Room("Skovens Hjerte", "Solens stråler filtrerer gennem løvtagets grene og kaster gyldne strejf over mosdækkede stier og levende træers hvisken.");
-        Room room7 = new Room("Hobbithjemmet", "Hyggelige huse gemmer sig blandt farverige blomsterenge og duftende køkkenhaver, hvor lyset danser på skæve stenterrasser.");
-        Room room8 = new Room("Dæmonisk Domstol", "Lavafloder omgiver en ruineret domstol, hvor dæmoniske væsner holder hof og udfælder skæbnesvangre domme under en flammende himmel.");
+        Room room7 = new Room("Home of Hobbit", "Cozy houses hide among colorful flower meadows and fragrant kitchen gardens, where light dances on crooked stone terraces.");
+        Room room8 = new Room("Demonic Court", "Lava rivers surround a ruined court, where demonic beings hold court and pronounce fateful judgments under a flaming sky.");
         Room room9 = new Room("Englernes Have", "En skyfri himmel bader engle i gyldent lys, mens blomsterbede stråler i alskens farver og en bæk risler mellem ædle sten.");
         firstRoom = room1;
         lastRoom = room5;
@@ -26,20 +26,20 @@ public class Map {
         //ROOM 1
         room1.setEast(room2);
         room1.setSouth(room4);
-        room1.addItemToCurrentRoom(new Food("vin", "En 100 år gammel rosindrik", -15));
-        room1.addItemToCurrentRoom(new Food("østers", "Håndplukket i østersøen", 30));
-        room1.addItemToCurrentRoom(new Food("egekilde", "Danskvand med citrus", 20));
-        room1.addItemToCurrentRoom(new MeleeWeapon("kniv", "Den vildeste kniv i byen", 10));
-        room1.addItemToCurrentRoom(new RangedWeapon("vandpistol", "Den sløveste vandpistol øst for Randers", 2, 15));
-        room1.addEnemyToCurrentRoom(new Enemy("Diablo", "Djævlen hamselv", 20, 10, new MeleeWeapon("Trefork", "Lucifers",10)));
+        room1.addItemToCurrentRoom(new Food("qine", "100 year old bottle", -15));
+        room1.addItemToCurrentRoom(new Food("oysters", "Handpicked in the supermarket", 30));
+        room1.addItemToCurrentRoom(new Food("sparklingwater", "With lime and citrus", 20));
+        room1.addItemToCurrentRoom(new MeleeWeapon("knife", "Crocodille Dundee would be jalous", 10));
+        room1.addItemToCurrentRoom(new RangedWeapon("watergun", "the cheap on from BR", 2, 15));
+        room1.addEnemyToCurrentRoom(new Enemy("Diablo", "The devil himself", 20, 10, new MeleeWeapon("Trefork", "Lucifers",10)));
 
         //ROOM 2
         room2.setWest(room1);
         room2.setEast(room3);
-        room2.addItemToCurrentRoom(new Food("muslibar", "Muslibar med tranebær, citron og hasselnødder", 5));
-        room2.addItemToCurrentRoom(new Food("bolle", "en bolle med ost og smør", 20));
-        room2.addItemToCurrentRoom(new RangedWeapon("dessertEagle", "Den tungeste dreng i byen", 25, 10));
-        room2.addEnemyToCurrentRoom(new Enemy("rasmuspaludan", "Med en koran og lighter i hånden", 50, 20, new MeleeWeapon("Lighter", "Den billige fra kiosken", 5)));
+        room2.addItemToCurrentRoom(new Food("granolabar", "A month of the date", 5));
+        room2.addItemToCurrentRoom(new Food("cheeseburger", "With a lot of pickles!", 20));
+        room2.addItemToCurrentRoom(new RangedWeapon("dessertEagle", "A heavy boy", 25, 10));
+        room2.addEnemyToCurrentRoom(new Enemy("rasmuspaludan", "With a coran and a lighter in the other hand", 50, 20, new MeleeWeapon("Lighter", "The cheap one", 5)));
 
         //ROOM 3
         room3.setWest(room2);
@@ -50,15 +50,16 @@ public class Map {
         //ROOM 4
         room4.setNorth(room1);
         room4.setSouth(room7);
-        room4.addItemToCurrentRoom(new Food("kakaodrik", "Supreme Chokomel drik", 15));
-        room4.addItemToCurrentRoom(new Food("grapefrugt", "En råden grapefrugt", -10));
-        room4.addItemToCurrentRoom(new MeleeWeapon("kølle", "Dolphs Kølle", 5));
+        room4.addItemToCurrentRoom(new Food("cocoadrink", "Supreme Chokomel drink", 15));
+        room4.addItemToCurrentRoom(new Food("grapefruit", "Rotten", -10));
+        room4.addItemToCurrentRoom(new MeleeWeapon("axe", "Yielded by a Look-A-Like Thor", 5));
 
         //ROOM 5
         room5.setSouth(room8);
-        room5.addItemToCurrentRoom(new Food("brikjuice", "Appelsinjuive", 15));
-        room5.addEnemyToCurrentRoom(new Enemy("BigBoss", "Last man standing", 100, 25, new MeleeWeapon(finalItemForTheFinalBoss(), "Grail", 0)));
+        room5.addItemToCurrentRoom(new Food("juice", "Orange without plums", 15));
+        room5.addEnemyToCurrentRoom(new Enemy("BigBoss", "El Chefe", 100, 25, new MeleeWeapon(finalItemForTheFinalBoss(), "Grail", 0)));
         //ROOM 6
+
         room6.setNorth(room3);
         room6.setSouth(room9);
         room6.addItemToCurrentRoom((new Food("fastelavnsbolle", "Den dyre fra Lagkagehuset", 20)));
@@ -69,17 +70,17 @@ public class Map {
         //ROOM 7
         room7.setNorth(room4);
         room7.setEast(room8);
-        room7.addItemToCurrentRoom(new Food("insekter", "Et glas insekter spicet op med BBQ", 10));
-        room7.addItemToCurrentRoom(new Food("kaffe", "En kop varm kaffe af mærket Merrild", 0));
-        room7.addEnemyToCurrentRoom(new Enemy("Matematiklærer", "En matematiklærer fra folkeskolen", 50, 15, new RangedWeapon("Matematikbøger", "10 års pensum af matematikbøger bliver kastet", 5, 10)));
+        room7.addItemToCurrentRoom(new Food("bugs", "Bugs, BBQ and Bourbon", 10));
+        room7.addItemToCurrentRoom(new Food("coffee", "A hot cup of Merrild Coffee", -5));
+        room7.addEnemyToCurrentRoom(new Enemy("mathteacher", "The old one from your elemantary schooll", 50, 15, new RangedWeapon("Algebrabooks", "Hardbacks", 5, 10)));
 
         //ROOM 8
         room8.setWest(room7);
         room8.setNorth(room5);
         room8.setEast(room9);
-        room8.addItemToCurrentRoom(new Food("energidrik", "Den helt billige sukkerfri fra Lidl", 5));
-        room8.addItemToCurrentRoom(new Food("flaske vand", "En halvtom,lun flaske kildevand", 5));
-        room8.addItemToCurrentRoom(new Enemy("Svigermor", "Hun er på besøg", 40,10, new MeleeWeapon("Emma Gad bog", "Hardback", 10)));
+        room8.addItemToCurrentRoom(new Food("energydrink", "The cheap non-brand one", 5));
+        room8.addItemToCurrentRoom(new Food("boooooowoahh", "A bootle of Water", 5));
+        room8.addItemToCurrentRoom(new Enemy("motherinlaw", "She's on a visit...", 40,10, new MeleeWeapon("A spandel", "The metal one", 10)));
 
         //ROOM 9
         room9.setWest(room8);
