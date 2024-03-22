22dad0969ae7443fa08d91f557dc17acb177501b
MartinEllebyBerg <160470389+MartinEllebyBerg@users.noreply.github.com>
2024-03-22 09:29:19 +0100

    Historie og smårettelser

--- src/Map.java
+++ src/Map.java
@@ -10,15 +10,15 @@ public class Map {
 
     //Methods
     public void createMap() {
-        Room room1 = new Room("The Dragon Chamber", "Glowing lava streams illuminate the eggs beneath the sky of scales and flames, while dragons rest in slumber.");
-        Room room2 = new Room("The Elf King's Salon)", "Crystal-like flowers bloom above swaying treetops, while elves dance among shimmering dust particles.");
-        Room room3 = new Room("Scyscraber of Magic", "The clouds envelop floating books, while starry sky archives and sky-blue shelves stretch towards the infinity of knowledge.");
-        Room room4 = new Room("Troldmandens Laboratorium", "Boblende kedler og glimtende eliksirer omgiver en hvirvlende portal, hvor magiens pulserende hjerte skaber et kalejdoskopisk skuespil.");
-        Room room5 = new Room("The Prison Cell", "The cell of horror, might and nothing but endless screaming, terror and pain. Welcome to hell!");
-        Room room6 = new Room("Skovens Hjerte", "Solens stråler filtrerer gennem løvtagets grene og kaster gyldne strejf over mosdækkede stier og levende træers hvisken.");
-        Room room7 = new Room("Home of Hobbit", "Cozy houses hide among colorful flower meadows and fragrant kitchen gardens, where light dances on crooked stone terraces.");
-        Room room8 = new Room("Demonic Court", "Lava rivers surround a ruined court, where demonic beings hold court and pronounce fateful judgments under a flaming sky.");
-        Room room9 = new Room("Englernes Have", "En skyfri himmel bader engle i gyldent lys, mens blomsterbede stråler i alskens farver og en bæk risler mellem ædle sten.");
+        Room room1 = new Room("The Moist Dungeon Floor", "As you begin your quest for the final boss, you feel the cold floor on your hands,\nand the voices echoes in the room as you try to stand up...\n");
+        Room room2 = new Room("The Storage Room", "You have passed a great, wooden door with the label 'Storage Room' on the front. As you turn on the lights\nan angry man is looking at you, phrassing things about religions...\n");
+        Room room3 = new Room("Entering the Matrix", "Aqua went back to the 80's, but you wil go back to 1999. On the floor, you see the colours Haiti.\nAs you come closer, you see two pills on a table - you have to pick, so choose wisely...\n");
+        Room room4 = new Room("The Forgotten Entry Room","You have arrivede in the Forgotten Entry Room. As you have beaten your first enemy,\nyou've gained strength, courage and items. Be prepared for the next!\n");
+        Room room5 = new Room("The Prison Cell", "The cell of horror, might and nothing but endless screaming, terror and pain. But luckily:\nEither you die here or will conquer the game... Welcome to hell!\n");
+        Room room6 = new Room("Entering the Animatrix", "Matrix wasn't a stand-alone thing, and neither will it be here. In front of you stands a great guy.\nA muscular guy, black shades, tight suit and a long coat...\n");
+        Room room7 = new Room("The Dining Room of Horror", "You arrive at The Dinning Room of Horror. A quick look around gives the ick, vomit in your mouth and a bad breath.\nEmpty meatsuits, heads and blood enough to take a swim in is covering the floor...\n");
+        Room room8 = new Room("The Demonic Court", "The castle usede to be an old court building 200 something years ago. Demons have risen through the cracked floors,\nand demons have taken over the court, the chairs and rulling. Beware of the encounters in the room!\n");
+        Room room9 = new Room("", "");
         firstRoom = room1;
         lastRoom = room5;
 
@@ -26,12 +26,12 @@ public class Map {
         //ROOM 1
         room1.setEast(room2);
         room1.setSouth(room4);
-        room1.addItemToCurrentRoom(new Food("qine", "100 year old bottle", -15));
+        room1.addItemToCurrentRoom(new Food("wine", "100 year old bottle", -15));
         room1.addItemToCurrentRoom(new Food("oysters", "Handpicked in the supermarket", 30));
         room1.addItemToCurrentRoom(new Food("sparklingwater", "With lime and citrus", 20));
         room1.addItemToCurrentRoom(new MeleeWeapon("knife", "Crocodille Dundee would be jalous", 10));
         room1.addItemToCurrentRoom(new RangedWeapon("watergun", "the cheap on from BR", 2, 15));
-        room1.addEnemyToCurrentRoom(new Enemy("Diablo", "The devil himself", 20, 10, new MeleeWeapon("Trefork", "Lucifers",10)));
+        room1.addEnemyToCurrentRoom(new Enemy("Diablo", "Spawn of satan", 20, 10, new MeleeWeapon("Trefork", "Lucifers",10)));
 
         //ROOM 2
         room2.setWest(room1);
@@ -53,11 +53,12 @@ public class Map {
         room4.addItemToCurrentRoom(new Food("cocoadrink", "Supreme Chokomel drink", 15));
         room4.addItemToCurrentRoom(new Food("grapefruit", "Rotten", -10));
         room4.addItemToCurrentRoom(new MeleeWeapon("axe", "Yielded by a Look-A-Like Thor", 5));
+        room4.addEnemyToCurrentRoom(new Enemy("mathteacher", "Standing with a shoehorn, a set of books preparing to yell at you", 50, 15, new RangedWeapon("Algebrabooks", "Hardbacks", 5, 10)));
 
         //ROOM 5
         room5.setSouth(room8);
         room5.addItemToCurrentRoom(new Food("juice", "Orange without plums", 15));
-        room5.addEnemyToCurrentRoom(new Enemy("BigBoss", "El Chefe", 100, 25, new MeleeWeapon(finalItemForTheFinalBoss(), "Grail", 0)));
+        room5.addEnemyToCurrentRoom(new Enemy("BigBoss", "El Chefe", 100, 30, new MeleeWeapon(finalItemForTheFinalBoss(), "Grail", 0)));
         //ROOM 6
 
         room6.setNorth(room3);
@@ -70,7 +71,8 @@ public class Map {
         room7.setEast(room8);
         room7.addItemToCurrentRoom(new Food("bugs", "Bugs, BBQ and Bourbon", 10));
         room7.addItemToCurrentRoom(new Food("coffee", "A hot cup of Merrild Coffee", -5));
-        room7.addEnemyToCurrentRoom(new Enemy("mathteacher", "The old one from your elemantary schooll", 50, 15, new RangedWeapon("Algebrabooks", "Hardbacks", 5, 10)));
+        room7.addEnemyToCurrentRoom(new Enemy("derekshepard", "The doctor of Greys Anatomy - ish", 50, 15, new MeleeWeapon("breadknife", "A very sharp knife", 10)));
+
 
         //ROOM 8
         room8.setWest(room7);
@@ -78,7 +80,7 @@ public class Map {
         room8.setEast(room9);
         room8.addItemToCurrentRoom(new Food("energydrink", "The cheap non-brand one", 5));
         room8.addItemToCurrentRoom(new Food("boooooowoahh", "A bootle of Water", 5));
-        room8.addItemToCurrentRoom(new Enemy("motherinlaw", "She's on a visit...", 40,10, new MeleeWeapon("A spandel", "The metal one", 10)));
+        room8.addItemToCurrentRoom(new Enemy("devilmotherinlaw", "She's awaiting trial for cooking a bad stew", 40,10, new MeleeWeapon("A spandel", "The metal one", 10)));
 
         //ROOM 9
         room9.setWest(room8);
